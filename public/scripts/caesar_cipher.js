ec2e75507d2c7d36ce34ec77015f50dfad7b1b27
Mohit Panthri <99413629+mohitpanthri@users.noreply.github.com>
2024-03-18 20:24:48 +0530

    Adding script for Caesar Cipher

--- public/scripts/caesar_cipher.js
+++ public/scripts/caesar_cipher.js
@@ -34,6 +34,10 @@ function isAlphabet(character) {
   return /[A-Z]/.test(character);
 }
 
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
 // Event handler for the Caesar Cipher buttons
 document.getElementById("caesarEncryptBtn").addEventListener("click", () => {
   const plainText = document.getElementById("caesarText").value.toUpperCase();
@@ -41,27 +45,48 @@ document.getElementById("caesarEncryptBtn").addEventListener("click", () => {
   const encryptedText = caesarEncrypt(plainText, shift);
   document.getElementById("caesarOutput").value = encryptedText;
 
-  if (plainText && shift) {
-    const solutionContainer = document.querySelector(".solution");
-    solutionContainer.innerHTML = "";
-    const solutionHeading = document.createElement("h5");
-    solutionHeading.innerText = `Solution Steps:
-  Encryption: C = (P + K)mod26`;
+  const solutionContainer = document.querySelector(".solution");
+  solutionContainer.innerHTML = "";
 
-    solutionContainer.appendChild(solutionHeading);
+  const indexTable = document.querySelector(".table-responsive");
+
+  if (plainText && shift && plainText.length <= 100) {
+    indexTable.classList.remove("hide");
+    solutionContainer.classList.remove("hide");
+    const solutionHeading1 = document.createElement("p");
+    solutionHeading1.innerText = `Solution Steps:`;
+    solutionHeading1.classList.add("steps-h1");
+    solutionContainer.appendChild(solutionHeading1);
+
+    const solutionHeading2 = document.createElement("p");
+    solutionHeading2.innerText = `Encryption: C = (P + K)mod26
+    where P -> Plain Text
+    \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0K -> Key
+    \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0C -> Cipher Text`;
+
+    solutionHeading2.classList.add("steps-h2");
+    solutionContainer.appendChild(solutionHeading2);
 
     for (let i = 0; i < plainText.length; i++) {
-      // console.log(plainText[i] + "->" + encryptedText[i]);
       if (isAlphabet(plainText[i])) {
         const steps = document.createElement("p");
 
-        steps.innerText = `For ${plainText[i]}:- (${
+        steps.innerText = `For ${
           plainText[i]
-        } + ${shift})mod26
-      (${plainText.charCodeAt(i) - 65} + ${shift})mod26
-      ${plainText.charCodeAt(i) - 65 + shift}mod26
-      ${(plainText.charCodeAt(i) - 65 + shift) % 26} i.e ${encryptedText[i]}`;
-
+        }:- \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 (${
+          plainText[i]
+        } + K)mod26
+      \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0(${
+        plainText.charCodeAt(i) - 65
+      } + ${shift})mod26
+      \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${
+        plainText.charCodeAt(i) - 65 + shift
+      }mod26
+      \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${mod(
+        plainText.charCodeAt(i) - 65 + shift,
+        26
+      )} i.e ${encryptedText[i]}`;
+        steps.classList.add("steps");
         solutionContainer.appendChild(steps);
       }
     }
@@ -73,29 +98,61 @@ document.getElementById("caesarDecryptBtn").addEventListener("click", () => {
   const shift = parseInt(document.getElementById("caesarShift").value);
   const decryptedText = caesarDecrypt(cipherText, shift);
   document.getElementById("caesarOutput").value = decryptedText;
+  const solutionContainer = document.querySelector(".solution");
+  solutionContainer.innerHTML = "";
 
-  if (cipherText && shift) {
-    const solutionContainer = document.querySelector(".solution");
-    solutionContainer.innerHTML = "";
-    const solutionHeading = document.createElement("h2");
-    solutionHeading.innerText = `Solution Steps:
-  Decryption: P = (C - K)mod26`;
+  if (cipherText && shift && cipherText.length <= 100) {
+    solutionContainer.classList.remove("hide");
+    const solutionHeading1 = document.createElement("p");
+    solutionHeading1.innerText = `Solution Steps:`;
+    solutionHeading1.classList.add("steps-h1");
+    solutionContainer.appendChild(solutionHeading1);
 
-    solutionContainer.appendChild(solutionHeading);
+    const solutionHeading2 = document.createElement("p");
+    solutionHeading2.innerText = `Decryption: P = (C - K)mod26
+    where C -> Cipher Text
+    \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0K -> Key
+    \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0P -> Plain Text`;
+    solutionHeading2.classList.add("steps-h2");
+    solutionContainer.appendChild(solutionHeading2);
 
     for (let i = 0; i < cipherText.length; i++) {
       if (isAlphabet(cipherText[i])) {
         const steps = document.createElement("p");
 
-        steps.innerText = `For ${cipherText[i]}:- (${
+        steps.innerText = `For ${
+          cipherText[i]
+        }:- \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 (${
           cipherText[i]
         } - ${shift})mod26
-      (${cipherText.charCodeAt(i) - 65} - ${shift})mod26
-      ${cipherText.charCodeAt(i) - 65 - shift}mod26
-      ${(cipherText.charCodeAt(i) - 65 - shift) % 26} i.e ${decryptedText[i]}`;
-
+      \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0(${
+        cipherText.charCodeAt(i) - 65
+      } - ${shift})mod26
+      \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${
+        cipherText.charCodeAt(i) - 65 - shift
+      }mod26
+      \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${mod(
+        cipherText.charCodeAt(i) - 65 - shift,
+        26
+      )} i.e ${decryptedText[i]}`;
+        steps.classList.add("steps");
         solutionContainer.appendChild(steps);
       }
     }
+  }
+});
+
+// Event handlers for Shift field
+document.querySelector("#increment").addEventListener("click", () => {
+  const shift = document.querySelector("#caesarShift");
+  if (shift.value >= 0) {
+    shift.stepUp();
+  }
+});
+
+document.querySelector("#decrement").addEventListener("click", () => {
+  const shift = document.querySelector("#caesarShift");
+  if (shift.value > 0) {
+    shift.stepDown();
   }
 });
